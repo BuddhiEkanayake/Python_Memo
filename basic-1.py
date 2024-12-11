f05bd4e484558d10b3b89f76c8cb2ea4e1af6638
BuddhiEkanayake <buddhisri12@gmail.com>
2024-12-11 10:31:15 +0530

    Created basic1.py
    Variable and receiving input

---
+++ basic-1.py
@@ -0,0 +1,17 @@
+name="John Smith"
+age=20
+print(name,age)
+
+#--------------------------------------Variables----------------------------------------------------------------------------------------------
+
+age=12
+first_name="John"
+
+#boolean values also we can store
+
+print(first_name)
+
+#-------------------------------------Receiving Input--------------------------------------------------------------------------------------------
+
+name=input("What is your name? ")
+print("Hello " + name)
