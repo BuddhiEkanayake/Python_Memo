3907858b399b200cae17f9112dd9b11c6ffb1825
BuddhiEkanayake <buddhisri12@gmail.com>
2024-12-12 09:04:15 +0530

    Update basic1.py
    Try some basics for memorize things and practice

--- basic-1.py
+++ basic-1.py
@@ -15,3 +15,35 @@ print(first_name)
 
 name=input("What is your name? ")
 print("Hello " + name)
+
+#------------------------------------Type Conversions-------------------------------------------------------------------------------------------------
+
+birth_year= input("What is your birth year? ")
+age=2024-int(birth_year)
+
+print( "my age is:" + str(age) )
+
+First_number= input("first number: ")
+Second_number= input("second number: ")
+
+Sum= int(First_number) + float(Second_number)
+
+print("SUM:"+ str(Sum))
+
+#------------------------------------String-----------------------------------------------------------------------------------------------------
+
+
+course= 'Python for Beginners'
+print(course.lower())
+
+print('y' in course) #true (we can get boolean values)
+
+print('for' in course)#true (we can get boolean values)
+
+print(course.find('for')) #7 (we can get index of the word)
+
+print(course.replace('Beginners','Absolute Beginners')) #Python for Absolute Beginners
+
+print(course.replace('for', '4')) #Python 4 Beginners
+
+print(course[0:3])
