271f4cd31f594043d62d10c344bcd4802cd2537d
BuddhiEkanayake <buddhisri12@gmail.com>
2025-03-07 23:59:20 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+for number in range(1,10,2):
+    print("Attemt", number+1,(number+1)*".")
+
+#--------If else with loop--------------------------------
+
+successful = False
+
+for number in range(3):
+    print("Attemt")
+
+    if successful:
+        print("Successful")
+        break
+    
+else:
+        print("Not successful")
+
+
+#-----------------Nested loops--------------------------------
+
+
+
