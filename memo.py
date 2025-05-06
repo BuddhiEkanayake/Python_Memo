ef29bec92bc9e2d79aacdafeb6964919f8cb5a38
BuddhiEkanayake <buddhisri12@gmail.com>
2025-05-06 08:10:51 +0530

    test

--- memo.py
+++ memo.py
@@ -1,4 +1,8 @@
 name="Buddhi"
 age=26
 
-print("My Name is-"+ name, "My Age is-" + age )
+print(f"My Name is- {name} My Age is- {age}" )
+
+print("My name is " + name,"my age is "+ str(age))
+
+print("Hi I'm",name,"and I'm",age,"years old")
